7cce6d72068c955eac5f59fe5540f98d22a2f25f
Jerolim Marcic <marca2marca3@gmail.com>
2020-02-10 00:45:25 +0100

    proba

--- Stranica/src/pages/examples/example1.js
+++ Stranica/src/pages/examples/example1.js
@@ -1,23 +1,8 @@
 import React from "react"
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap"
-
+import GlavniDio from "../../components/header"
 const Example1 = () => {
-  return (
-    <div>
-      <Navbar bg="primary" variant="dark" fixed="top">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-light">Search</Button>
-        </Form>
-      </Navbar>
-    </div>
-  )
+  return <GlavniDio></GlavniDio>
 }
 
 export default Example1
