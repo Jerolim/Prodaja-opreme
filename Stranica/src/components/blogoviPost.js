c432d90a9c33933b60ec6d94553290354f083e9a
Jerolim Marcic <marca2marca3@gmail.com>
2020-02-16 18:02:21 +0100

    popravak greske bloga

--- Stranica/src/components/blogoviPost.js
+++ Stranica/src/components/blogoviPost.js
@@ -47,77 +47,77 @@ const Postovi = [
       "The Vietnam War was a war fought between 1964 and 1975 on the ground in South Vietnam and bordering areas of Cambodia and Laos (see also, Secret War), and in bombing runs (Rolling Thunder) over North Vietnam. Fighting on one side was a coalition of forces including the United States (USA), the Republic of Vietnam (South Vietnam), Australia, and South Korea. Fighting on the other side was a coalition of forces including the Democratic Republic of Vietnam (North Vietnam) and the National Liberation Front (NLF, Viet Cong), a communist-led South Vietnamese guerrilla movement. The USSR provided military aid to the North Vietnamese and to the NLF, but was not one of the military combatants.",
   },
   {
-    Naslov: "Fall of Rome",
+    Naslov: "Fall of Rome.",
     TekstPosta:
       "The invading army reached the outskirts of Rome, which had been left totally undefended. In 410 C.E., the Visigoths, led by Alaric, breached the walls of Rome and sacked the capital of the Roman Empire. The Visigoths looted, burned, and pillaged their way through the city, leaving a wake of destruction wherever they went. The plundering continued for three days. For the first time in nearly a millennium, the city of Rome was in the hands of someone other than the Romans. This was the first time that the city of Rome was sacked, but by no means the last.",
   },
   {
-    Naslov: "Rise of British Empire",
+    Naslov: "Rise of British Empire.",
     TekstPosta:
       "The British began to establish overseas colonies in the 16th century. By 1783, Britain had a large empire with colonies in America and the West Indies. This 'first British Empire' came to an end after the American Revolution. However, in the 19th century, the British built a second worldwide empire, based on British sea-power, made up of India and huge conquests in Africa.",
   },
   {
-    Naslov: "Tank tactics in Soviet Union",
+    Naslov: "Tank tactics in Soviet Union.",
     TekstPosta:
       "At the start of the Second World War much of the Red Army, including its armoured forces, was in transition and recovering from the 1937 repression of the officer corps. The Red Army ignored the lessons from Nomonhan, which had been successfully conducted by General Zhukov, and relied instead on lessons from politically selected officers who were veterans of the Spanish Civil War.",
   },
   {
-    Naslov: "Future of warfare",
+    Naslov: "Future of warfare.",
     TekstPosta:
       "There are robots on the market that can carry and fire weapons like shotguns, pepper spray, grenade launchers, or even Hellfire missiles. The MULE ARV-A-L robot can fire a line-of-sight gun and anti-tank weaponry. Remote controlled TALON robots can carry everything from an M240 machine gun to a .50 caliber rifle to grenades and rocket launchers. The South Korean patrol robot can either fire non-lethal rubber bullets at intruders or carry a K-3 machine gun -- a light machine gun similar to the M249. The U.S. Marine Corps' Gladiator Tactical Unmanned Ground Vehicle (TUGV) will be able to carry an arsenal of lethal and non-lethal weapons.",
   },
   {
-    Naslov: "Vietnam war",
+    Naslov: "Vietnam war.",
     TekstPosta:
       "The Vietnam War was a war fought between 1964 and 1975 on the ground in South Vietnam and bordering areas of Cambodia and Laos (see also, Secret War), and in bombing runs (Rolling Thunder) over North Vietnam. Fighting on one side was a coalition of forces including the United States (USA), the Republic of Vietnam (South Vietnam), Australia, and South Korea. Fighting on the other side was a coalition of forces including the Democratic Republic of Vietnam (North Vietnam) and the National Liberation Front (NLF, Viet Cong), a communist-led South Vietnamese guerrilla movement. The USSR provided military aid to the North Vietnamese and to the NLF, but was not one of the military combatants.",
   },
   {
-    Naslov: "Fall of Rome",
+    Naslov: "Fall of Rome..",
     TekstPosta:
       "The invading army reached the outskirts of Rome, which had been left totally undefended. In 410 C.E., the Visigoths, led by Alaric, breached the walls of Rome and sacked the capital of the Roman Empire. The Visigoths looted, burned, and pillaged their way through the city, leaving a wake of destruction wherever they went. The plundering continued for three days. For the first time in nearly a millennium, the city of Rome was in the hands of someone other than the Romans. This was the first time that the city of Rome was sacked, but by no means the last.",
   },
   {
-    Naslov: "Rise of British Empire",
+    Naslov: "Rise of British Empire..",
     TekstPosta:
       "The British began to establish overseas colonies in the 16th century. By 1783, Britain had a large empire with colonies in America and the West Indies. This 'first British Empire' came to an end after the American Revolution. However, in the 19th century, the British built a second worldwide empire, based on British sea-power, made up of India and huge conquests in Africa.",
   },
   {
-    Naslov: "Tank tactics in Soviet Union",
+    Naslov: "Tank tactics in Soviet Union..",
     TekstPosta:
       "At the start of the Second World War much of the Red Army, including its armoured forces, was in transition and recovering from the 1937 repression of the officer corps. The Red Army ignored the lessons from Nomonhan, which had been successfully conducted by General Zhukov, and relied instead on lessons from politically selected officers who were veterans of the Spanish Civil War.",
   },
   {
-    Naslov: "Future of warfare",
+    Naslov: "Future of warfare..",
     TekstPosta:
       "There are robots on the market that can carry and fire weapons like shotguns, pepper spray, grenade launchers, or even Hellfire missiles. The MULE ARV-A-L robot can fire a line-of-sight gun and anti-tank weaponry. Remote controlled TALON robots can carry everything from an M240 machine gun to a .50 caliber rifle to grenades and rocket launchers. The South Korean patrol robot can either fire non-lethal rubber bullets at intruders or carry a K-3 machine gun -- a light machine gun similar to the M249. The U.S. Marine Corps' Gladiator Tactical Unmanned Ground Vehicle (TUGV) will be able to carry an arsenal of lethal and non-lethal weapons.",
   },
   {
-    Naslov: "Vietnam war",
+    Naslov: "Vietnam war..",
     TekstPosta:
       "The Vietnam War was a war fought between 1964 and 1975 on the ground in South Vietnam and bordering areas of Cambodia and Laos (see also, Secret War), and in bombing runs (Rolling Thunder) over North Vietnam. Fighting on one side was a coalition of forces including the United States (USA), the Republic of Vietnam (South Vietnam), Australia, and South Korea. Fighting on the other side was a coalition of forces including the Democratic Republic of Vietnam (North Vietnam) and the National Liberation Front (NLF, Viet Cong), a communist-led South Vietnamese guerrilla movement. The USSR provided military aid to the North Vietnamese and to the NLF, but was not one of the military combatants.",
   },
   {
-    Naslov: "Fall of Rome",
+    Naslov: "Fall of Rome...",
     TekstPosta:
       "The invading army reached the outskirts of Rome, which had been left totally undefended. In 410 C.E., the Visigoths, led by Alaric, breached the walls of Rome and sacked the capital of the Roman Empire. The Visigoths looted, burned, and pillaged their way through the city, leaving a wake of destruction wherever they went. The plundering continued for three days. For the first time in nearly a millennium, the city of Rome was in the hands of someone other than the Romans. This was the first time that the city of Rome was sacked, but by no means the last.",
   },
   {
-    Naslov: "Rise of British Empire",
+    Naslov: "Rise of British Empire...",
     TekstPosta:
       "The British began to establish overseas colonies in the 16th century. By 1783, Britain had a large empire with colonies in America and the West Indies. This 'first British Empire' came to an end after the American Revolution. However, in the 19th century, the British built a second worldwide empire, based on British sea-power, made up of India and huge conquests in Africa.",
   },
   {
-    Naslov: "Tank tactics in Soviet Union",
+    Naslov: "Tank tactics in Soviet Union...",
     TekstPosta:
       "At the start of the Second World War much of the Red Army, including its armoured forces, was in transition and recovering from the 1937 repression of the officer corps. The Red Army ignored the lessons from Nomonhan, which had been successfully conducted by General Zhukov, and relied instead on lessons from politically selected officers who were veterans of the Spanish Civil War.",
   },
   {
-    Naslov: "Future of warfare",
+    Naslov: "Future of warfare...",
     TekstPosta:
       "There are robots on the market that can carry and fire weapons like shotguns, pepper spray, grenade launchers, or even Hellfire missiles. The MULE ARV-A-L robot can fire a line-of-sight gun and anti-tank weaponry. Remote controlled TALON robots can carry everything from an M240 machine gun to a .50 caliber rifle to grenades and rocket launchers. The South Korean patrol robot can either fire non-lethal rubber bullets at intruders or carry a K-3 machine gun -- a light machine gun similar to the M249. The U.S. Marine Corps' Gladiator Tactical Unmanned Ground Vehicle (TUGV) will be able to carry an arsenal of lethal and non-lethal weapons.",
   },
   {
-    Naslov: "Vietnam war",
+    Naslov: "Vietnam war...",
     TekstPosta:
       "The Vietnam War was a war fought between 1964 and 1975 on the ground in South Vietnam and bordering areas of Cambodia and Laos (see also, Secret War), and in bombing runs (Rolling Thunder) over North Vietnam. Fighting on one side was a coalition of forces including the United States (USA), the Republic of Vietnam (South Vietnam), Australia, and South Korea. Fighting on the other side was a coalition of forces including the Democratic Republic of Vietnam (North Vietnam) and the National Liberation Front (NLF, Viet Cong), a communist-led South Vietnamese guerrilla movement. The USSR provided military aid to the North Vietnamese and to the NLF, but was not one of the military combatants.",
   },
